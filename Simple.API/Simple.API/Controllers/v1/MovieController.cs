f1d032ea137192b610c757d3e1524983bbca3760
Paulo Henrique Berti Garbelini <paulo.berti@iterative.com.br>
2022-12-01 13:22:51 -0300

    First

---
+++ Simple.API/Simple.API/Controllers/v1/MovieController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Simple.API.Controllers.v1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : BaseController
+    {
+    }
+}
---
+++ Simple.API/Simple.Api.Application/Service/Base/BaseService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+
+namespace Simple.Api.Application.Service.Base
+{
+    public class BaseService<T>
+    {
+        protected readonly ILogger<T> _logger; 
+        protected readonly IMapper _mapper;
+        protected BaseService(ILogger<T> logger, IMapper mapper)
+        {
+            _logger = logger;
+            _mapper = mapper;
+        }
+    }
+}
